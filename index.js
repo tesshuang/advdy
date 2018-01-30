273a65492f706f6a675edc736a5ab4c84f06b518
Tess <tesshuangxj@yahoo.com>
2018-01-30 15:01:38 -0800

    edited

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 //public port for heroku: process.env.PORT || 3000
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req,resp)=>{
     resp.end("Hello to my page.");
